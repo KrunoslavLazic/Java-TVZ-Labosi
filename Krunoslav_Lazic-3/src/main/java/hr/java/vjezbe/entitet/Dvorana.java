c200c8687b93e857732e3c92d665e63afe899543
Krunoslav Lazic <118220949+KrunoslavLazic@users.noreply.github.com>
2023-03-30 00:34:09 +0200

    Delete Krunoslav_Lazic-3 directory

--- Krunoslav_Lazic-3/src/main/java/hr/java/vjezbe/entitet/Dvorana.java
+++
@@ -1,9 +0,0 @@
-package hr.java.vjezbe.entitet;
-
-/**
- * Record za novu dvoranu
- * @param ime ime dvorane
- * @param zgrada ime zgrade
- */
-public record Dvorana(String ime, String zgrada) {
-}
--- Krunoslav_Lazic-3/src/main/java/hr/java/vjezbe/entitet/Online.java
+++
@@ -1,5 +0,0 @@
-package hr.java.vjezbe.entitet;
-
-public sealed interface Online permits Ispit{
-    String softwareZaIspit(String imeSoftwarea);
-}
